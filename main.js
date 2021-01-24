47939c492b29fa311ac6b77046477d513c3b43c4
Marek Rawdanowicz <marek.rawdanowicz@outlook.com>
2021-01-24 14:38:28 +0100

    add path

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const fs = require("fs");
+const path = require("path");
 
 const nodemailer = require("nodemailer");
 
@@ -17,35 +18,40 @@ app.post("/", (req, res, next) => {
   const log = `${date.toLocaleString("pl-PL").replace(/\,/g, "")}\r\n`;
   const logFile = date.toLocaleDateString("pl-PL");
 
-  fs.appendFile(`./logs/${logFile}.log`, log, (err) => {
-    if (!err && logFile) {
-      console.log(log);
+  fs.appendFile(
+    path.resolve(__dirname, "logs", `${logFile}.log`),
+    log,
+    (err) => {
+      if (!err && logFile) {
+        console.log(log);
 
-      const mail = {
-        from: `Marek Rawdanowicz ${process.env.EMAIL_SENDER}`,
-        to: `${process.env.EMAIL_RECEIVER}`,
-        subject: `${logFile}.log`,
-        text: "Somebody just accessed rawdanowiczdev.pl, log file attached.",
-        attachments: [
-          {
-            path: `./logs/${logFile}.log`,
-          },
-        ],
-      };
+        const mail = {
+          from: `Marek Rawdanowicz ${process.env.EMAIL_SENDER}`,
+          to: `${process.env.EMAIL_RECEIVER}`,
+          subject: `${logFile}.log`,
+          text: "Somebody just accessed rawdanowiczdev.pl, log file attached.",
+          attachments: [
+            {
+              filename: `${logFile}.log`,
+              path: path.resolve(__dirname, "logs", `${logFile}.log`),
+            },
+          ],
+        };
 
-      transporter.sendMail(mail, (err) => {
-        if (!err) {
-          console.log(`Logs sent to ${mail.to}`);
-        } else {
-          console.log(err);
-        }
-      });
-    } else {
-      console.log(err);
+        transporter.sendMail(mail, (err) => {
+          if (!err) {
+            console.log(`Logs sent to ${mail.to}`);
+          } else {
+            console.log(err);
+          }
+        });
+      } else {
+        console.log(err);
+      }
     }
-  });
+  );
 
   res.status(201).end();
 });
 
-app.listen(3000);
+app.listen(3100);
